a76fe8bc63ef883da1cc5b4b6669067020c67035
AndreD4 <andremai629@gmail.com>
2023-06-14 12:41:01 +0900

    boostspeed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed =30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>();
+       surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     
     void Update()
-    {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-          rb2d.AddTorque(torqueAmount);
-        }
+  {
+    RotatePlayer();
+    RespondToBoost();
+  }
 
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-          rb2d.AddTorque(-torqueAmount);
-        }
+  void RespondToBoost()
+  {
+      if(Input.GetKey(KeyCode.UpArrow))
+      {
+        surfaceEffector2D.speed = boostSpeed;
+      }
+      else
+      {
+        surfaceEffector2D.speed = baseSpeed;
+      }
+  }
+
+  void RotatePlayer()
+  {
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      rb2d.AddTorque(torqueAmount);
     }
+
+    if (Input.GetKey(KeyCode.RightArrow))
+    {
+      rb2d.AddTorque(-torqueAmount);
+    }
+  }
 }
